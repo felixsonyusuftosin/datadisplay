855ccc99ce41de8a4a7307d3756f00f70d262c76
ftosin@botkeeper.com <felixsonyusuftosin@gmail.com>
2020-11-16 01:57:00 +0100

    updates

--- src/components/dataDisplay/body/DataRow.tsx
+++ src/components/dataDisplay/body/DataRow.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../commons/Columns.css'
 import { Card, TransactionItem } from '../../../types'
 import './DataRow.css'
 import { formatCurrency$ } from '../../../utils/currency'
 import { formatDate } from '../../../utils/dates'
 import Checkbox from '../header/Checkbox'
+
+enum ImageOptions {
+  masterCard = 'https://avatars.sched.co/7/e0/7507288/avatar.jpg?c05',
+  visa = 'https://e7.pngegg.com/pngimages/594/666/png-clipart-visa-logo-credit-card-debit-card-payment-card-bank-visa-blue-text.png'
+}
 
 export const DataRow: React.FC<TransactionItem> = (item: TransactionItem) => {
   const {
@@ -19,18 +24,43 @@ export const DataRow: React.FC<TransactionItem> = (item: TransactionItem) => {
     card
   }: TransactionItem = item
   const { scheme }: Card = card
+  const [selected, setSelected ] = useState(false)
+
+  const schemaImage = () => {
+    switch (scheme) {
+      case 'mastercard':
+        return ImageOptions.masterCard
+      case 'visa':
+        return ImageOptions.visa
+      default:
+        return ''
+    }
+  }
+  const toggleSelection = (): void => {
+    setSelected(!selected)
+  }
+
   return (
-    <div className='table-column data-row'>
-       <Checkbox />
+    <div className='table-column data-row' onClick={toggleSelection}>
+      <Checkbox checked={selected} onChange={toggleSelection}/>
       <div className='column'>{currency}</div>
       <div className='column'>{auth ? 'Authorized' : 'Not Authorized'}</div>
-      <div className='column'>{formatCurrency$(amount, currency)}</div>
+      <div className='column bolder dark-text'>
+        {formatCurrency$(amount, currency)}
+      </div>
       <div className='column'>{accountId}</div>
-      <div className='column'>{cleared ? 'Cleared' : 'Not Cleared'}</div>
+      <div className='column  bolder dark-text align-center'>
+        {cleared ? 'Cleared' : 'Not Cleared'}
+      </div>
       <div className='column'>{wallet ? wallet : '-'}</div>
       <div className='column'>{offer ? offer : '-'}</div>
-      <div className='column'>{formatDate(new Date(datetime))}</div>
-      <div className='column'>{scheme}</div>
+      <div className='column column  bolder dark-text '>
+        {formatDate(new Date(datetime))}
+      </div>
+      <div className='column center'>
+        <img src={schemaImage()} className='card-type-image' alt='schema' />
+        {/* <span>{scheme}</span> */}
+      </div>
     </div>
   )
 }
