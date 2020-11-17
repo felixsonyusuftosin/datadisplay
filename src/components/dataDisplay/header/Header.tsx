3abe92db5cb33595888069d43093b1165979594c
ftosin@botkeeper.com <felixsonyusuftosin@gmail.com>
2020-11-17 16:54:21 +0100

    lint Header

--- src/components/dataDisplay/header/Header.tsx
+++ src/components/dataDisplay/header/Header.tsx
@@ -9,7 +9,7 @@ import TableHead from './TableHead'
 import { useDataDisplay } from '../../context/DataDisplay.context'
 import { ContextState, FilterType, NotificationTypes } from '../../../types'
 import { getLastYear, getWordDate, getLastMonth } from '../../../utils/dates'
-import Notification  from '../../../commons/Notification'
+import Notification from '../../../commons/Notification'
 
 const Search = ({ ...props }) => {
   const [searchTerm, setSearchTerm] = useState('')
@@ -43,45 +43,47 @@ const Search = ({ ...props }) => {
   )
 }
 
-type HeaderProps = { 
+type HeaderProps = {
   toggleFilter: () => void
 }
 
-export const Header: React.FC<HeaderProps> = ({ toggleFilter}) => {
+export const Header: React.FC<HeaderProps> = ({ toggleFilter }) => {
   const { state, setFilter } = useDataDisplay() as any
   const { filter } = state as ContextState
   const { from, to } = filter as FilterType
   const localDateReadableFormatFrom = getWordDate(from)
-  const  localDateReadableFormatTo = getWordDate(to)
+  const localDateReadableFormatTo = getWordDate(to)
 
-  const [filterVisibilityState, setFilterVisibilityState ] = useState({ 
+  const [filterVisibilityState, setFilterVisibilityState] = useState({
     matchesLastMonth: getLastMonth(new Date()) === from,
-    matchesLastYear:  getLastYear(new Date()) === from
+    matchesLastYear: getLastYear(new Date()) === from
   })
 
-  const handleFilterLastYearChanged = () => { 
+  const handleFilterLastYearChanged = () => {
     const fromDate = getLastYear(new Date())
-      setFilter({
-        ...filter,
-        from: fromDate,
-        to: new Date()
-      })  
-     setFilterVisibilityState({ matchesLastMonth: false, matchesLastYear: true})
+    setFilter({
+      ...filter,
+      from: fromDate,
+      to: new Date()
+    })
+    setFilterVisibilityState({ matchesLastMonth: false, matchesLastYear: true })
   }
-  
 
-  const handleFilterLastMonthChanged = () => { 
+  const handleFilterLastMonthChanged = () => {
     const fromDate = getLastMonth(new Date())
     setFilter({
       ...filter,
       from: fromDate,
       to: new Date()
     })
-    setFilterVisibilityState({ matchesLastMonth: true, matchesLastYear: false})
+    setFilterVisibilityState({ matchesLastMonth: true, matchesLastYear: false })
   }
 
-  const handleCustomSelection = () => { 
-    setFilterVisibilityState({ matchesLastMonth: false, matchesLastYear: false})
+  const handleCustomSelection = () => {
+    setFilterVisibilityState({
+      matchesLastMonth: false,
+      matchesLastYear: false
+    })
     toggleFilter()
   }
 
@@ -97,18 +99,33 @@ export const Header: React.FC<HeaderProps> = ({ toggleFilter}) => {
           Transactions
         </Typography>
         <Typography variant='h6' className='light-text' gutterBottom>
-          '  '
+          ' '
         </Typography>
         <div className='button-row-container'>
           <div className='curved-button-row'>
-            <button onClick={handleFilterLastMonthChanged} className={matchesLastMonth ? 'blue white-text' : 'white dark-text'}>Last Month</button>
-            <button onClick={handleFilterLastYearChanged} className={matchesLastYear ? 'blue white-text' : 'white dark-text'}>Last Year</button>
-            <button className='white dark-text' onClick={handleCustomSelection}> Custom</button>
+            <button
+              onClick={handleFilterLastMonthChanged}
+              className={
+                matchesLastMonth ? 'blue white-text' : 'white dark-text'
+              }>
+              Last Month
+            </button>
+            <button
+              onClick={handleFilterLastYearChanged}
+              className={
+                matchesLastYear ? 'blue white-text' : 'white dark-text'
+              }>
+              Last Year
+            </button>
+            <button className='white dark-text' onClick={handleCustomSelection}>
+              {' '}
+              Custom
+            </button>
           </div>
         </div>
         <div className='details-container light-text'>
           <Typography variant='body1' className='dark-text truncate bolder'>
-           {`Results from ${localDateReadableFormatFrom} To ${localDateReadableFormatTo}`}
+            {`Results from ${localDateReadableFormatFrom} To ${localDateReadableFormatTo}`}
           </Typography>
           <FilterListIcon onClick={toggleFilter} className='filter dark-text' />
           <GetAppIcon className='filter dark-text' />
